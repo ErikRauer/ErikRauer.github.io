7ce46a106da7218d7b75654f87efab082bfa68cb
Erik Rauer <eriktr22@gmail.com>
2017-04-01 22:05:21 +0000

    Tic Tac Toe Game now checks for whether or not a player has won

--- tic_tac_toe/main.js
+++ tic_tac_toe/main.js
@@ -3,9 +3,9 @@ var currentTurn = true;
 
 //Field data
 //[row][column][Whether the cell is owned, Who owns the cell, Top left X value, Top left Y value]
-var rows = [[[false, ""], [false, ""], [false, ""]], 
-            [[false, ""], [false, ""], [false, ""]], 
-            [[false, ""], [false, ""], [false, ""]]];
+var rows = [[[false, "1"], [false, "2"], [false, "3"]], 
+            [[false, "4"], [false, "5"], [false, "6"]], 
+            [[false, "7"], [false, "8"], [false, "9"]]];
 // var topRow = [topL, topM, topR];
 // var middleRow = [middleL, middleM, middleR];
 // var bottomRow = [bottomL, bottomM, bottomR];
@@ -24,12 +24,14 @@ var rows = [[[false, ""], [false, ""], [false, ""]],
 
 //Called when the player clicks on a cell
 function clickCell(row, column){
+    //If the cell is already used, write text
     if (rows[row][column][0] == true){
         textSize(32);
         text("Cell is already in use", 25, 25);
     } else {
         strokeWeight(5);
         
+        //If it's O's turn, draw the circle in the correct position
         if (currentTurn){
             switch(rows[row][column]){
                 case rows[0][0]:
@@ -64,7 +66,10 @@ function clickCell(row, column){
                     text("Error", 25, 25);
             }
             rows[row][column][1] = "O";
+            //And change who's turn it is
             currentTurn = false;
+            
+        //If it's X's turn, draw the cross in the correct position
         } else {
             switch(rows[row][column]){
                 case rows[0][0]:
@@ -108,6 +113,7 @@ function clickCell(row, column){
                     text("Error", 25, 25);
             }
             rows[row][column][1] = "X";
+            //And change whos turn it is
             currentTurn = true;
         }
         
@@ -115,8 +121,43 @@ function clickCell(row, column){
     }
 }
 
+//Check if someone has won the game yet. If yes, display text.
+function checkForWin() {
+    textSize(32);
+    if (rows[0][0][1] == rows[0][1][1] && rows[0][0][1] == rows[0][2][1]){
+        text(rows[0][0][1] + " Won the Game!" , 75, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[1][0][1] == rows[1][1][1] && rows[1][0][1] == rows[1][2][1]){
+        text(rows[1][0][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[2][0][1] == rows[2][1][1] && rows[2][0][1] == rows[2][2][1]){
+        text(rows[2][0][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[0][0][1] == rows[1][0][1] && rows[0][0][1] == rows[2][0][1]){
+        text(rows[0][0][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[0][1][1] == rows[1][1][1] && rows[0][1][1] == rows[2][1][1]){
+        text(rows[0][1][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[0][2][1] == rows[1][2][1] && rows[0][2][1] == rows[2][2][1]){
+        text(rows[0][2][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[0][2][1] == rows[1][2][1] && rows[0][2][1] == rows[2][2][1]){
+        text(rows[0][2][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[0][0][1] == rows[1][1][1] && rows[0][0][1] == rows[2][2][1]){
+        text(rows[0][0][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } else if (rows[2][0][1] == rows[1][1][1] && rows[2][0][1] == rows[0][2][1]){
+        text(rows[2][0][1] + " Won the Game!" , 100, 100);
+        text("Reload page to restart", 50, 130);
+    } 
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    
+    //Draw the field
     line(width/3, 0, width/3, height);
     line((width/3)*2, 0, (width/3)*2, height);
     line(0, height/3, width, height/3);
@@ -124,9 +165,11 @@ function setup() {
 }
 
 function draw() {
+    checkForWin();
 }
 
 function mouseClicked() {
+    //The position of the mouse when it's clicked will determine which cell is used
     if (mouseX < width/3 && mouseY < height/3){
         clickCell(0,0);
     } else if(mouseX < width/3 && mouseY < (height/3)*2){
